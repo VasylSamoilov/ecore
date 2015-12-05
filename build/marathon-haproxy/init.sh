a82976457ce16b116e979084a4dc2f31564fe502
Oleksii <nixlike@gmail.com>
2015-12-05 11:45:22 +0200

    submodule cleanup

--- build/marathon-haproxy/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./haproxy-marathon-bridge $MARATHON_IP >/haproxy.cfg && /usr/sbin/haproxy -bd -f /haproxy.cfg 
-while sleep 2
-do  
-	./haproxy-marathon-bridge $MARATHON_IP >/haproxy.cfg
-	/usr/sbin/haproxy -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
-done
