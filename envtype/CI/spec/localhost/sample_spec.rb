c39a13cfe4657b61349d667f04e14d063dcc5056
Oleksii Dzhulai <nixlike@gmail.com>
2016-02-16 18:26:37 +0200

    Fixed test definition

--- envtype/CI/spec/localhost/sample_spec.rb
+++ envtype/CI/spec/localhost/sample_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 #Minimal test for an instance during provision
-describe command('docker run --entrypoint bash nixlike/buildcont:ubuntu-15.10 -c "cd ecore/envtype/CI && rake" ') do
-                  its(:stdout) { should not match /Error/ }
+describe command(' "cd ecore/envtype/CI && rake" 2>&1 ') do
+                   its(:exit_status) { should eq 0 }
 end
