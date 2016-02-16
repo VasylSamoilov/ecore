7ca4d1635bc19a8ead07ed7f4d6653b45e624404
Oleksii Dzhulai <nixlike@gmail.com>
2016-02-16 18:39:48 +0200

    Further test fixing

--- envtype/CI/spec/localhost/sample_spec.rb
+++ envtype/CI/spec/localhost/sample_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
 #Minimal test for an instance during provision
-describe command(' "curl -f http://leader.mesos:5050/master/health" 2>&1 ') do
+describe command('curl -f http://leader.mesos:5050/master/health 2>&1') do
                    its(:exit_status) { should eq 0 }
 end
